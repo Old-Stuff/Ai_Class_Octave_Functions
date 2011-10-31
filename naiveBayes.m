3d2f66f432dfca02f6c82d6f716394ffc07d0968
Myles Borins <myles.borins@gmail.com>
2011-10-31 04:50:28 -0400

    Finished naiveBayes.m

--- naiveBayes.m
+++ naiveBayes.m
@@ -11,8 +11,8 @@ l2 = cellstr(l2);
 query = cellstr(query);
 
 % The first and simplest step will be calculating the probability of the Lists using Laplacian Smoothing via k
-p_l1 = (size(l1) + k) / (size(l1) + size(l2) + 2);
-p_l2 = (size(l1) + k) / (size(l1) + size(l2) + 2);
+p_l1 = (length(l1) + k) / (length(l1) + length(l2) + 2);
+p_l2 = (length(l2) + k) / (length(l1) + length(l2) + 2);
 
 % Print to screen the results
 fprintf('P(List_1): %f\n', p_l1);
@@ -31,8 +31,25 @@ size_dict = length(dict); % add variable for size of dictionary
 % Prepare Query For Calculations
 query_ind = strsplit(char(query), " "); % break up query into individual words
 size_query = length(query_ind); % get size of query
+pq_gl=zeros(size_query,2);
+
+%Set probabilities for query | lists
+pq_top = [p_l1;p_l2];
 
 % Calculate Probability of words in query for each list
+for i = 1:size_query
+	pq_gl(i,1) = (occurence(query_ind(i),l1_words) + k) / (size_l1 + size_dict);
+	pq_gl(i,2) = (occurence(query_ind(i),l2_words) + k) / (size_l2 + size_dict);
+	fprintf('%s | List_1 = %f\n', query_ind{i}, pq_gl (i,1));
+	fprintf('%s | List_2 = %f\n', query_ind{i}, pq_gl (i,2));
+	pq_top(1) = pq_top(1) * pq_gl(i,1);
+	pq_top(2) = pq_top(2) * pq_gl(i,2);
+end
 
+pq_l1 = pq_top(1) / (pq_top(1) + pq_top(2));
+pq_l2 = pq_top(2) / (pq_top(1) + pq_top(2));
+
+fprintf('%s | List_1 = %f\n', query{1}, pq_l1);
+fprintf('%s | List_2 = %f\n', query{1}, pq_l2);
 	
 end
