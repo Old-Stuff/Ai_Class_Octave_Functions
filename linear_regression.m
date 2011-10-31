1696c8ec028671bbb1ee29ea16cb5ac25f9e0e19
Myles Borins <myles.borins@gmail.com>
2011-10-31 00:12:42 -0400

    Added Function to solve for w1

---
+++ linear_regression.m
@@ -0,0 +1,15 @@
+function [w0, w1] = linReg(x, y)
+
+% Function to calculate W0 and W1 of two vectors based on Ai-Class Lectures
+
+% Initialize some useful values
+m = length(y); %number of training examples
+
+% You need to return the following variables correctly
+w0 = 0;
+w1 = 0;
+
+w1 = ((m*(x'*y)) - (sum(x) * sum(y))) / ((m * sum(x.^2)) - sum(x)^2);
+
+
+end
